405cd21fd131cd2c21e245df4ae20281e2248851
yr6 <45499641+yr6@users.noreply.github.com>
2018-12-03 00:46:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1381,5 +1381,68 @@ try {
     console.error(e);
   };
 }
+client.music = require("discord.js-musicbot-addon");
+
+// Now we start the music module.
+client.music.start(client, {
+  // Set the api key used for YouTube.
+  // This is required to run the bot.
+  youtubeKey: "AIzaSyD8G7jHMxTAtPeVGvRxG4xgIuTLjrXcdPQ"
+});
+
+client.music.start(client, {
+  // Set the api key used for YouTube.
+  youtubeKey: "AIzaSyD8G7jHMxTAtPeVGvRxG4xgIuTLjrXcdPQ",
+
+  // The PLAY command Object.
+  play: {
+    // Usage text for the help command.
+    usage: "ٌ!p",
+    // Whether or not to exclude the command from the help command.
+    exclude: false  
+  },
+
+  // Make it so anyone in the voice channel can skip the
+  // currently playing song.
+  anyoneCanSkip: true,
+
+  // Make it so the owner (you) bypass permissions for music.
+  ownerOverMember: true,
+  ownerID: "246220920199446528",
+
+  // The cooldown Object.
+  cooldown: {
+    // This disables the cooldown. Not recommended.
+    enabled: false
+  }
+});
+
+client.on("message", (msg) => {
+  if (msg.author.bot) return; // Good practice to do this.
+
+  // I set the Client to this just for ease.
+  // You'll probably have access to it another
+  // way, but this still works.
+  const client = msg.client;
+
+  // Get the command from the message.
+  const command = message.substring(musicbot.botPrefix.length).split(/[ \n]/)[0].trim();
+
+  // Get the suffix, the String after the command.
+  const suffix = message.substring(musicbot.botPrefix.length + command.length).trim();
+
+  // Set the prefix to "!". This is a horrible way to set
+  // one, but it will do for now.
+  let prefix = "r!"
+
+  // Now we check if the message starts with the prefix,
+  // and asks for the PLAY command.
+  if (msg.content.startsWith(prefix) && command == "play") {
+    // Now we pass the Message Object (msg) and
+    // the suffix. It will then proceed as it would
+    // with the bot normally.
+    client.music.bot.playFunction(msg, suffix);
+  };
+});
 
 client.login(process.env.bitch);
